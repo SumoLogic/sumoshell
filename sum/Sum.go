dd8ce64f6e2ad3462116405c5693ac3d2683c330
Russell Cohen <russell@sumologic.com>
2015-10-14 13:54:46 -0700

    Add missing sum

---
+++ sum/Sum.go
@@ -0,0 +1,67 @@
+package sum
+
+import (
+	"fmt"
+	"github.com/SumoLogic/sumoshell/group"
+	"github.com/SumoLogic/sumoshell/util"
+	"strconv"
+)
+
+type sum struct {
+	sum *float64
+	key string
+	// DO NOT MODIFY BASE
+	base   map[string]interface{}
+	output func(map[string]interface{})
+}
+
+func makeSum(key string) sum {
+	sumV := 0.0
+	return sum{&sumV, key, make(map[string]interface{}), util.NewJsonWriter().Write}
+
+}
+
+func aggregateSum(output grouper.Merger, key string, base map[string]interface{}) util.SumoAggOperator {
+	sumV := 0.0
+	return sum{&sumV, key, base, output.Write}
+}
+
+func Build(args []string) (util.SumoAggOperator, error) {
+	// Fist arg is "sum"
+	args = args[1:]
+	if len(args) == 1 {
+		return makeSum(args[0]), nil
+	} else if len(args) > 1 {
+		key := args[0]
+		//_ := relevantArgs[1]
+		keyFields := args[2:]
+		return grouper.NewAggregate(aggregateSum, keyFields, key), nil
+	} else {
+		return nil, util.ParseError("Need a argument to average (`sum field`)")
+	}
+}
+
+func (sumOp sum) Flush() {
+	sumOp.output(util.CreateStartRelation())
+	sumOp.output(util.CreateRelation(currentState(sumOp)))
+	sumOp.output(util.CreateEndRelation())
+}
+
+func currentState(s sum) map[string]interface{} {
+	ret := make(map[string]interface{})
+	for key, val := range s.base {
+		ret[key] = val
+	}
+	ret["_sum"] = *s.sum
+	return ret
+}
+
+func (s sum) Process(inp map[string]interface{}) {
+	v, keyInMap := inp[s.key]
+	if keyInMap {
+		f, keyIsNumber := strconv.ParseFloat(fmt.Sprint(v), 64)
+		if keyIsNumber == nil {
+			*s.sum += f
+		}
+	}
+}
